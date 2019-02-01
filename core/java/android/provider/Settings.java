31253fb1c60540a1b2ac12b91dcec01ca3311cc5
Marko Man <darkobas@gmail.com>
2019-02-01 15:43:13 +0100

    Add option to enable AOD on charging only [1/2]
    abcdu edit: move to Secure and use existing systemui tuner settings
    observer in DozeParameters
    
    Change-Id: I95bf93e7902e9cedc02726403b9a6ab111c7c669

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9429,6 +9429,18 @@ public final class Settings {
          * @hide
          */
         public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
+
+         /**
+         * Whether to enable DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE = "doze_on_charge";
+
+        /**
+         * hidden stting of the current atate of DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE_NOW = "doze_on_charge_now";
     }
 
     /**
