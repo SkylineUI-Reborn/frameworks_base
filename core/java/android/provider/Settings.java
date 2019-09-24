a847ee4f726f2e6de7c072dc2e036e8b2400b0b5
Marko Man <darkobas@gmail.com>
2019-09-24 14:46:01 +0200

    SystemUI: Double tap to sleep on statusbar [1/2]
    arrow-11.0 edits:
    - Adapt code to refactored statusbar controllers.
    
    wave edits:
    - Adapt to existing dt2s on lockscreen
    
    [ghostrider-reborn | imjyotiraditya: adapt to android 12]
    
    [ghostrider-reborn]
    - Adapt to android 13-qpr2
    - Rework and cleanup code
    - Also fixup c458902 ("Make DT2S on lockscreen depend on DT2W state")
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Change-Id: I4a4a543a5c045fa1a6456bb23d04bbe6a0dcf4a3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5521,6 +5521,12 @@ public final class Settings {
          */
 
         /**
+         * Whether to sleep the device on double tap on statusbar
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5649,6 +5655,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
             PRIVATE_SETTINGS.add(DESKTOP_MODE);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
         }
 
         /**
