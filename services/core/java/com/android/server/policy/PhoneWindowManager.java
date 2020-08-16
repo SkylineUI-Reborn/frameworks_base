e8df5777450b16f5fb57a5ce6b0eea4322faa669
Cosmin Tanislav <demonsingur@gmail.com>
2020-08-16 19:45:00 +0200

    services: unlock fingerprint only on power button press
    Change-Id: I2f569a8ab4d1dd2c118cfb2d1c1072ce6b4e3584

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -388,6 +388,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     final Object mServiceAquireLock = new Object();
     Vibrator mVibrator; // Vibrator for giving feedback of orientation changes
     SearchManager mSearchManager;
+    private boolean interceptPowerKeyAuthOrEnroll = false;
+    private long interceptPowerKeyTimeByFinger = -1;
+    private boolean isAuthenOrEnrollRunningWhenDown = false;
     AccessibilityManager mAccessibilityManager;
     BurnInProtectionHelper mBurnInProtectionHelper;
     private DisplayFoldController mDisplayFoldController;
@@ -626,6 +629,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Fallback actions by key code.
     private final SparseArray<KeyCharacterMap.FallbackAction> mFallbackActions =
             new SparseArray<KeyCharacterMap.FallbackAction>();
+
+    private WindowManagerPolicy.FingerListener mFingerListener;
 
     private final LogDecelerateInterpolator mLogDecelerateInterpolator
             = new LogDecelerateInterpolator(100, 0);
@@ -1068,6 +1073,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         schedulePossibleVeryLongPressReboot();
 
+        isAuthenOrEnrollRunningWhenDown = interceptPowerKeyAuthOrEnroll;
+
         // If the power key has still not yet been handled, then detect short
         // press, long press, or multi press and decide what to do.
         mPowerKeyHandled = hungUp || mScreenshotChordVolumeDownKeyTriggered
@@ -1269,6 +1276,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      * @return True if the was device was sent to sleep, false if sleep was suppressed.
      */
     private boolean goToSleepFromPowerButton(long eventTime, int flags) {
+        if ((eventTime - interceptPowerKeyTimeByFinger < 700 || isAuthenOrEnrollRunningWhenDown) && Utils.hasPowerButtonFingerprint(mContext)) {
+            return false;
+        }
         // Before we actually go to sleep, we check the last wakeup reason.
         // If the device very recently woke up from a gesture (like user lifting their device)
         // then ignore the sleep instruction. This is because users have developed
@@ -2814,6 +2824,21 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
         };
 
+    /**
+     * Fingerprint unlock on press
+     */
+    public void registerFingerListener(WindowManagerPolicy.FingerListener listener) {
+        mFingerListener = listener;
+    }
+
+    public void interceptPowerKeyByFinger(long time) {
+        interceptPowerKeyTimeByFinger = time;
+    }
+
+    public void notifySideFpAuthenOrEnroll(boolean start) {
+        interceptPowerKeyAuthOrEnroll = start;
+    }
+
     // TODO(b/117479243): handle it in InputPolicy
     /** {@inheritDoc} */
     @Override
@@ -4144,6 +4169,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_POWER: {
+                if (mFingerListener != null) {
+                    mFingerListener.powerDown(down);
+                }
                 EventLogTags.writeInterceptPower(
                         KeyEvent.actionToString(event.getAction()),
                         mPowerKeyHandled ? 1 : 0, mPowerKeyPressCounter);
@@ -4789,6 +4817,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void wakeUpFromPowerKey(long eventTime) {
+        if ((eventTime - this.interceptPowerKeyTimeByFinger < 700) && Utils.hasPowerButtonFingerprint(mContext)) {
+            return;
+        }
         wakeUp(eventTime, mAllowTheaterModeWakeFromPowerKey,
                 PowerManager.WAKE_REASON_POWER_BUTTON, "android.policy:POWER");
     }
