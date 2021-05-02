c08cf9127067167b28a28cb3706c723b06fdb4cb
TikkiTikki <radstylemusic@gmail.com>
2021-05-02 18:04:37 -0300

    Add Android S Clocks

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -222,8 +222,9 @@ public class KeyguardSliceProvider extends SliceProvider implements
         // Show header if music is playing and the status bar is in the shade state. This way, an
         // animation isn't necessary when pressing power and transitioning to AOD.
         boolean keepWhenShade = mStatusBarState == StatusBarState.SHADE && mMediaIsVisible;
-        return !TextUtils.isEmpty(mMediaTitle) && mMediaIsVisible && (mDozing || keepWhenAwake
-                || keepWhenShade);
+        boolean isAndroidSClockSelected = currentClock == null ? false : currentClock.contains("Android") && currentClock.contains("S");
+        return !TextUtils.isEmpty(mMediaTitle) && (mMediaIsVisible || isAndroidSClockSelected) && (mDozing || keepWhenAwake
+                ||keepWhenShade || isAndroidSClockSelected) && isAndroidSClockSelected;
     }
 
     protected void addMediaLocked(ListBuilder listBuilder) {
