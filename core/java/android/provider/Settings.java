773f5456c4d60a060e7334fa0dc0a52d83152ae2
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-05-14 00:53:09 +0200

    AutoBrightness: Add support for one shot auto-brightness
    - Only update auto brightness one time when screen is turned on.
    - Can be useful on devices where sensor is not accurate.
    
    Author: Eamon Powell <eamonpowell@outlook.com>
    Date:   Sun Aug 29 11:37:44 2021 +1000
    
        fixup! AutoBrightness: Add support for one shot auto-brightness:
    
        * Remove unnecessary newlines
        * Fix incorrect indentation
        * Fix typo in comments
    
        Change-Id: I7d6071f4344605b3dcc3f1c33ac4390f1020e03f
    
    Change-Id: I5187fdb7765550d4eaa999317ab95f487d44c8d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5884,6 +5884,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Whether auto brightness is applied one shot when screen is turned on
+         * @hide
+         */
+        public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6051,6 +6057,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION);
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION_COLOR);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
+            PRIVATE_SETTINGS.add(AUTO_BRIGHTNESS_ONE_SHOT);
         }
 
         /**
