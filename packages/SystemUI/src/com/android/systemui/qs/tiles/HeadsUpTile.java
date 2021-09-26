aefc4d0eb4711c3f128b0d51fdd202f6eef5695b
Shivansh Agarwal <shivansh.aeyron@gmail.com>
2021-09-26 22:49:00 +0530

    Fix METRIC

--- packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
@@ -111,7 +111,7 @@ public class HeadsUpTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.PIXELISH;
+        return MetricsEvent.PIXEL;
     }
 
     @Override
