6374f469b144a94b3dee113ab34457f5fb16d917
Danny Lin <danny@kdrag0n.dev>
2021-11-01 20:06:48 -0700

    Limit SafetyNet workarounds to unstable GMS process
    The unstable process is where SafetyNet attestation actually runs, so
    we only need to spoof the model in that process. Leaving other processes
    fixes various issues caused by model detection and flag provisioning,
    including screen-off Voice Match in Google Assistant, broken At a Glance
    weather and settings on Android 12, and more.
    
    Change-Id: Idcf663907a6c3d0408dbd45b1ac53c9eb4200df8

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -15,6 +15,7 @@
  */
 package com.android.internal.util.custom;
 
+import android.app.Application;
 import android.os.Build;
 import android.os.SystemProperties;
 import android.util.Log;
@@ -120,12 +121,17 @@ public class PixelPropsUtils {
         propsToChangePixelXL.put("FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys");
     }
 
-    public static void setProps(String packageName) {
+    public static void setProps(Application app) {
+        final String packageName = app.getPackageName();
+        final String processName = app.getProcessName();
+
         if (packageName == null) {
             return;
         }
-        if (packageName.equals(PACKAGE_GMS)) {
-            sIsGms = true;
+
+        if (packageName.equals(PACKAGE_GMS) &&
+                    processName.equals(PACKAGE_GMS + ".unstable")) {
+                sIsGms = true;
         }
         if (packageName.equals(PACKAGE_NETFLIX) && !SystemProperties.getBoolean(
                 "persist.pixelpropsutils.spoof_netflix", true)) {
