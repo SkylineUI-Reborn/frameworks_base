3d47cfa8a36f7ef329ac8eb4d1ec685c61f81451
jhenrique09 <jhenrique09@users.noreply.github.com>
2022-01-18 20:46:03 +0800

    base: Implement cutout force full screen [1/2]
    Inspired by MIUI and Essential
    
    CutoutFullscreenController: Adapted from https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    
    [cjh1249131356]: Adapt to A12
    
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Sat, 21 Apr 2018 23:45:57 -0700
    
        Add an option to force pre-O apps to use full screen aspect ratio
    
        When an app target pre-O releases, the default max aspect ratio
        is 1.86:1 which leads to ugly black areas on devices that have
        screens with higher aspect ratio (for example Galaxy S8/S9).
    
        This change adds an option to allow users to change aspect ratio
        for pre-O apps to full screen aspect ratio.
    
        Change-Id: I41d5408841593a12443be885e11959bffaebb67b
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: Idba39c0594eecb0a43f33fd97368f0e52d0dffbb
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5607,6 +5607,12 @@ public final class Settings {
         public static final String FINGERPRINT_WAKE_UNLOCK = "fingerprint_wake_unlock";
 
         /**
+         * Force full screen for devices with cutout
+         * @hide
+         */
+        public static final String FORCE_FULLSCREEN_CUTOUT_APPS = "force_full_screen_cutout_apps";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -5756,6 +5762,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
+            PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
         }
 
         /**
--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -271,6 +271,8 @@ import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.usage.AppStandbyInternal;
 
+import com.android.internal.util.custom.cutout.CutoutFullscreenController;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -782,6 +784,8 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     public AppStandbyInternal mAppStandbyInternal;
 
+    private CutoutFullscreenController mCutoutFullscreenController;
+
     private final class SettingObserver extends ContentObserver {
         private final Uri mFontScaleUri = Settings.System.getUriFor(FONT_SCALE);
         private final Uri mHideErrorDialogsUri = Settings.Global.getUriFor(HIDE_ERROR_DIALOGS);
@@ -878,6 +882,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     public void installSystemProviders() {
         mSettingsObserver = new SettingObserver();
+
+        // Force full screen for devices with cutout
+        mCutoutFullscreenController = new CutoutFullscreenController(mContext);
     }
 
     public void retrieveSettings(ContentResolver resolver) {
@@ -6880,6 +6887,12 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
                 activity.restartProcessIfVisible();
             }
+        }
+    }
+
+    public boolean shouldForceCutoutFullscreen(String packageName) {
+        synchronized (this) {
+            return mCutoutFullscreenController.shouldForceCutoutFullscreen(packageName);
         }
     }
 }
