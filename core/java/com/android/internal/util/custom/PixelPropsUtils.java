572d7ef466b6cc8f2eb59eb976ea91c9dcaef042
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-29 23:05:49 +0530

    PixelPropsUtils: Allow disabling netflix spoofing
    Set persist.pixelpropsutils.spoof_netflix to true to
    opt in of netflix spoofing if, say, the device supports
    Dolby Vision or has HDR.
    
    Change-Id: I0a62bd64d92d71401f862c4edc99077dc8241c5b

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -35,6 +35,7 @@ public class PixelPropsUtils {
     private static final String DEVICE = "org.pixelexperience.device";
     private static final boolean DEBUG = false;
 
+    public static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
     private static final String SAMSUNG = "com.samsung.android.";
 
     private static final Map<String, Object> propsToChange;
@@ -59,7 +60,7 @@ public class PixelPropsUtils {
             "com.android.vending",
             "com.breel.wallpapers20",
             "com.nothing.smartcenter",
-            "com.netflix.mediaclient"
+            PACKAGE_NETFLIX
     };
 
     private static final String[] packagesToKeep = {
@@ -133,6 +134,11 @@ public class PixelPropsUtils {
             return;
         }
         if (Arrays.asList(packagesToKeep).contains(packageName)) {
+            return;
+        }
+        if (packageName.equals(PACKAGE_NETFLIX) && !SystemProperties.getBoolean(
+                "persist.pixelpropsutils.spoof_netflix", false)) {
+            if (DEBUG) Log.d(TAG, "Netflix spoofing disabled by system prop");
             return;
         }
         if (packageName.startsWith("com.google.")
