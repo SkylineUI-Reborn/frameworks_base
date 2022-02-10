b8ecb461ce6c7adb7d5071cf953cf3d5dc4ae340
Michael P <palaych@arrowos.net>
2022-02-10 06:12:29 +0000

    SystemUI: Fix method for disabling unlock ripple animation
    NotificationShadeWindowController's are not gracefully closed, which
    leads to top parts of the screen is being obscured by invisible shade window.
    And also, "ripple" is actually still exists due to lightRevealScrim are
    being started by onKeyguardFadingAwayChanged, which is not what user expect
    when they want to disable the effect.
    
    So in order to handle those cases, lets intercept it from the
    AuthRippleController instead.
    
    Change-Id: I85ab96368c8b0099700f3de18ba95ce63eaac443
    Signed-off-by: Ashwin.D.S <astroashwin@outlook.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
@@ -87,6 +87,9 @@ class AuthRippleController @Inject constructor(
     private var aodExpandedDwellScale = 2.3f
     private var udfpsRadius: Float = -1f
 
+    private val skipRipple: Boolean 
+        get() = Settings.System.getIntForUser(sysuiContext.getContentResolver(), Settings.System.SKIP_FP_UNLOCK_ANIMATION, 0, UserHandle.USER_CURRENT) == 1
+
     override fun onInit() {
         mView.setAlphaInDuration(sysuiContext.resources.getInteger(
                 R.integer.auth_ripple_alpha_in_duration).toLong())
@@ -125,11 +128,6 @@ class AuthRippleController @Inject constructor(
             return
         }
 
-        if (biometricSourceType == BiometricSourceType.FINGERPRINT && Settings.System.getIntForUser(sysuiContext.getContentResolver(),
-                Settings.System.SKIP_FP_UNLOCK_ANIMATION, 0, UserHandle.USER_CURRENT) == 1 ) {
-            return
-        }
-
         if (biometricSourceType == BiometricSourceType.FINGERPRINT &&
             fingerprintSensorLocation != null) {
             mView.setSensorLocation(fingerprintSensorLocation!!)
@@ -145,6 +143,11 @@ class AuthRippleController @Inject constructor(
     }
 
     private fun showUnlockedRipple() {
+        
+        if (skipRipple) {
+            return
+        }
+        
         notificationShadeWindowController.setForcePluginOpen(true, this)
         val useCircleReveal = circleReveal != null && biometricUnlockController.isWakeAndUnlock
         val lightRevealScrim = statusBar.lightRevealScrim
@@ -162,6 +165,14 @@ class AuthRippleController @Inject constructor(
     }
 
     override fun onKeyguardFadingAwayChanged() {
+        if (skipRipple) {
+            // reset and hide the scrim so it doesn't appears on
+            // the next notification shade usage
+            statusBar.lightRevealScrim?.revealAmount = 1f
+            startLightRevealScrimOnKeyguardFadingAway = false
+            return
+        }
+        
         if (keyguardStateController.isKeyguardFadingAway) {
             val lightRevealScrim = statusBar.lightRevealScrim
             if (startLightRevealScrimOnKeyguardFadingAway && lightRevealScrim != null) {
