081d648dcf7a472b202e64af2ec9f553f3461172
Karan Parashar <whyredfire@gmail.com>
2022-05-21 11:49:31 +0530

    !fixup: Global Actions with Advanced Reboot

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -1034,7 +1034,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             if (!mRebootMenu && showRebootSubmenu()) {
                 mRebootMenu = true;
                 mCurrentMenuActions = mRebootMenuActions;
-                handleShow();
+                handleShow(null);
             } else {
                 mHandler.sendEmptyMessage(MESSAGE_DISMISS);
                 doReboot();
