e91c8d56f276dc59b29de22612cb395f43735077
jhenrique09 <jhsv09@gmail.com>
2022-06-08 01:44:36 +0000

    PixelPropsUtils: Disable spoof for recorder app
    Change-Id: If0e8cc40483455666aac0abb57da99d53df57f8c
    (cherry picked from commit ec941056af6633cddbe7564d43a118b0d631f61e)

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -40,7 +40,6 @@ public class PixelPropsUtils {
     private static final String[] packagesToChangePixel5 = {
             "com.android.vending",
             "com.google.android.apps.photos",
-            "com.google.android.apps.recorder",
             "com.google.android.apps.turbo",
             "com.google.android.apps.turboadapter",
             "com.google.android.dialer",
@@ -75,7 +74,8 @@ public class PixelPropsUtils {
         "com.google.android.UltraCVM",
         "com.google.android.apps.cameralite",
         "com.google.ar.core",
-        "com.google.android.apps.wearables.maestro.companion"
+        "com.google.android.apps.wearables.maestro.companion",
+        "com.google.android.apps.recorder"
     };
 
     // Codenames for currently supported Pixels by Google
