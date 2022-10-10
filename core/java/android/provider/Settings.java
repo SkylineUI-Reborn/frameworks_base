8475a81f50938ee01f03529ec3f74cfe22ed897a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-10 23:06:57 +0530

    SystemUI: Allow disabling screenshot shutter sound [2/2]
    Change-Id: If653666f4bf6ab6600943c0b27d6dc4dab6461ca

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5913,6 +5913,12 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Whether to play the camera shutter sound on taking a screenshot.
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
