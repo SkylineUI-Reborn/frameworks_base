a44dc1017e2a61ae6f8e1af87afb302393f2ac66
basamaryan <basam.aryan@gmail.com>
2023-02-07 00:06:37 +0000

    PixelPropsUtils: Update fingerprint from February
    Change-Id: I4515a4b8117dbdd6209664c909367646a17c0b04

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -45,7 +45,7 @@ public class PixelPropsUtils {
         "DEVICE", "redfin",
         "PRODUCT", "redfin",
         "MODEL", "Pixel 5",
-        "FINGERPRINT", "google/redfin/redfin:13/TQ1A.230105.001/9292298:user/release-keys"
+        "FINGERPRINT", "google/redfin/redfin:13/TQ1A.230205.002/9471150:user/release-keys"
     );
 
     private static final Map<String, Object> sPixelXLProps = Map.of(
