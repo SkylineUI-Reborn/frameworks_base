5e0c219b634148b5cfc25bfefc0befbcb102f9f0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-05-31 22:23:06 +0530

    SystemUI: SoundTile: Open volume panel on long press
    Change-Id: I33fc152770cf668b5713221e65241d1ca0068762

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
@@ -119,7 +119,7 @@ public class SoundTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return new Intent(Settings.ACTION_SOUND_SETTINGS);
+        return new Intent(Settings.Panel.ACTION_VOLUME);
     }
 
     @Override
