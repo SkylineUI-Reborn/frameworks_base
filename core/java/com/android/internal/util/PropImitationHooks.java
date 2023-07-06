69d60fe21de9b7d76b7cb39cfe8f34ca2b3ea9ec
basamaryan <basam.aryan@gmail.com>
2023-07-06 05:01:12 +0200

    PIHooks: Update fingerprints from July 2023
    Change-Id: I2cc8aa36a4fc23fd78d5dc631abf5009b1012a83

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -75,7 +75,7 @@ public class PropImitationHooks {
         "DEVICE", "cheetah",
         "PRODUCT", "cheetah",
         "MODEL", "Pixel 7 Pro",
-        "FINGERPRINT", "google/cheetah/cheetah:13/TQ3A.230605.012/10204971:user/release-keys"
+        "FINGERPRINT", "google/cheetah/cheetah:13/TQ3A.230705.001/10216780:user/release-keys"
     );
 
     private static final Map<String, Object> sPixelProps = Map.of(
@@ -84,7 +84,7 @@ public class PropImitationHooks {
         "DEVICE", "redfin",
         "PRODUCT", "redfin",
         "MODEL", "Pixel 5",
-        "FINGERPRINT", "google/redfin/redfin:13/TQ3A.230605.011/10161073:user/release-keys"
+        "FINGERPRINT", "google/redfin/redfin:13/TQ3A.230705.001/10216780:user/release-keys"
     );
 
     private static final ComponentName GMS_ADD_ACCOUNT_ACTIVITY = ComponentName.unflattenFromString(
