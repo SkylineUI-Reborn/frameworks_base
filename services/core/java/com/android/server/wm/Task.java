252a695be8b0911d4345b4b3633969e2419e4b12
buwenjing <buwenjing@xiaomi.com>
2024-01-04 11:43:23 +0800

    Reset top activity's mWaitForEnteringPinnedMode when aborting an incomplete pip-entry
    When aborting an incomplete pip-entry, reset top activity mWaitForEnteringPinnedMode to prevent skipping it's window frame computation later.
    
    Bug: 318553345
    Test: Manual test
    
    Change-Id: I9378f54957534c2fab7d877d03bd40d2573959da
    Signed-off-by: buwenjing <buwenjing@xiaomi.com>

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -4740,6 +4740,7 @@ class Task extends TaskFragment {
         }
         if (top.isAttached()) {
             top.setWindowingMode(WINDOWING_MODE_UNDEFINED);
+            top.mWaitForEnteringPinnedMode = false;
         }
     }
 
